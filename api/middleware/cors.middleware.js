1f8ef340106bec1e98032920e211ee0fee72909d
Matias <floresmatias0@gmail.com>
2021-04-14 19:32:40 -0300

    CORS-FIX-V2

--- api/middleware/cors.middleware.js
+++ api/middleware/cors.middleware.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 server.use(cors({
   credentials: true,
-  origin: 'http://104.236.200.20/' || "*"
+  origin: "http://104.236.92.12" || "*"
 }));
 
 module.exports = server;
